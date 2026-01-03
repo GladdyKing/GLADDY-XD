60eb2d406b487da9ed2f39f0f39b31a860a53c6e
Gladdy King <davisongladson6@gmail.com>
2026-01-03 16:02:21 +0200

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,10 @@
+//=====👑 ɢʟᴀᴅᴅʏ  xᴅ 🧵====
+
+module.exports = {
+  BOT_NAME: 'ɢʟᴀᴅᴅʏ xᴅ', // ur bot name
+  OWNER_NAME: 'ɢʟᴀᴅᴅʏ ᴋɪɴɢ', //your name
+  OWNER_NUMBER: '263775335753', // add your phone number
+  SESSION_ID: '', // add your session ID
+  MODE: "private", // public or private
+  TIMEZONE: 'Africa/Harare' // ur timezone
+};
